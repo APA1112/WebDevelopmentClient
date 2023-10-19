3da9754120cd62745f673cec9a1d4dbee206191b
APA1112 <alejandropatonaraujo2@gmail.com>
2023-10-19 14:36:11 +0200

    ej_javascript_23 v_final

---
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_23/ej_javascript_23.js
@@ -0,0 +1,16 @@
+function irWeb(){
+    var opciones = document.getElementsByName("web");
+    var link = null;
+
+    for (var i = 0; i < opciones.length; i++) {
+        if (opciones[i].checked) {
+            link = opciones[i].value;
+            break;
+        }
+    }
+    if (link) {
+        window.location.href = link;
+    } else {
+        alert("Por favor, selecciona una página web.");
+    }
+}
