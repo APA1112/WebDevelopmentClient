99d7192f944ae2507c316b0388fdbfe6ddd845c2
APA1112 <alejandropatonaraujo2@gmail.com>
2023-10-19 14:06:26 +0200

    ej_javascript_22 sin event

--- EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_22/ej_javascript_22.js
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_22/ej_javascript_22.js
@@ -3,15 +3,15 @@ function main() {
   console.log(elemento);
 
   //Añadimos un EventListener para cuando el raton este sobre el div cambie
-  elemento.addEventListener("mouseover", function (event) {
-    event.target.style.backgroundColor = "blue";
-    event.target.style.color = "yellow";
+  elemento.addEventListener("mouseover", function () {
+    elemento.style.backgroundColor = "blue";
+    elemento.style.color = "yellow";
   });
 
   //EventListener que devuelve el div a su estado original
-  elemento.addEventListener("mouseout", function (event) {
-    event.target.style.backgroundColor = "";
-    event.target.style.color = "";
+  elemento.addEventListener("mouseout", function () {
+    elemento.style.backgroundColor = "";
+    elemento.style.color = "";
   });
 }
 
