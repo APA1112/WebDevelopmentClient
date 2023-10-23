028a56b17bdc93f032f5351b4bc226df53dd828f
Alejandro <apatara1112@g.educaand.es>
2023-10-23 12:59:09 +0200

    ej_javascript_25 modified

--- EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_25/ej_javascript_25.js
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_25/ej_javascript_25.js
@@ -1,6 +1,6 @@
 function irWeb(){
     var lista = document.querySelector('select [name = "paginas"]');
-    var seleccion = lista.options[lista.selectedIndex];
+    var seleccion = lista.selectedIndex;
     if (seleccion) {
         var url = seleccion.value;
         window.open(url, '_blank');
