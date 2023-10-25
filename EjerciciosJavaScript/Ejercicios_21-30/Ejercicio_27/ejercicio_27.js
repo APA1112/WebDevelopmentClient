d62edc22d880f9f5029697d0019699a8948e4164
Alejandro <apatara1112@g.educaand.es>
2023-10-25 08:36:22 +0200

    ejercicio_27 final

--- EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_27/ejercicio_27.js
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_27/ejercicio_27.js
@@ -1,7 +1,12 @@
-var resolucionActual = window.screen.width + "x" + window.screen.height;
-var radios = document.querySelectorAll('input[name="resolucion"]');
+function main(){
+  var resolucionActual = window.screen.width + "x" + window.screen.height;
+var radios = document.getElementsByName('resolucion');
 radios.forEach(function (radio) {
   if (radio.value === resolucionActual) {
     radio.checked = true;
   }
 });
+}
+
+
+window.addEventListener("DOMContentLoaded", main);
