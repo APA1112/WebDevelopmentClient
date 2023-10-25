93490279ea9e22f39c44545c67bf3510ca2e649a
Alejandro <apatara1112@g.educaand.es>
2023-10-25 09:42:49 +0200

    ej_javascript_26 terminado

--- EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_26/ej_javascript_26.js
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_26/ej_javascript_26.js
@@ -1,10 +1,10 @@
 function irWeb() {
   var lista = document.getElementById("webs");
   var seleccion = lista.selectedIndex;
-  if (seleccion > 0) {
+  if (seleccion) {
     console.log(lista.value);
     window.open(lista.value, "_blank");
   }
 }
 
-window.addEventListener("DOMContentLoaded", irWeb);
+window.addEventListener("change", irWeb);
