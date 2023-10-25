421d2831cb1d001d44d6cd9c8c561a8951fbfe6c
Alejandro <apatara1112@g.educaand.es>
2023-10-25 09:27:15 +0200

    ej_javascript_26

--- EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_26/ej_javascript_26.js
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_26/ej_javascript_26.js
@@ -1,15 +1,10 @@
-function main() {
-  var boton = document.getElementById("open");
-  boton.addEventListener("click", irWeb);
-}
-
 function irWeb() {
   var lista = document.getElementById("webs");
   var seleccion = lista.selectedIndex;
-  if (seleccion) {
+  if (seleccion > 0) {
     console.log(lista.value);
     window.open(lista.value, "_blank");
   }
 }
 
-window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("DOMContentLoaded", irWeb);
