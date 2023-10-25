45673cc3a61f83c93f6c99895bcf3d548174a4b5
Alejandro <apatara1112@g.educaand.es>
2023-10-25 10:24:14 +0200

    ejercicio_27 mid term

--- EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_27/ejercicio_27.js
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_27/ejercicio_27.js
@@ -1,11 +1,20 @@
+const resoluciones = [
+  "1920x1080",
+  "1680x1050",
+  "1600x1024",
+  "1600x900",
+  "1440x1080",
+];
 function main() {
   var resolucionActual = window.screen.width + "x" + window.screen.height;
+  console.log(resolucionActual);
   var radios = document.getElementsByName("resolucion");
-  radios.forEach(function (radio) {
-    if (radio.value === resolucionActual) {
+  for (radio of radios) {
+    if (resolucionActual === resoluciones[radio.value]) {
       radio.checked = true;
+    }else{
+      
     }
-  });
+  }
 }
-
 window.addEventListener("DOMContentLoaded", main);
