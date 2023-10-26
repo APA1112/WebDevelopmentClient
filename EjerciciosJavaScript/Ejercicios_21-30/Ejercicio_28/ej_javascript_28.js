194851add50a4e28e537357cc51456da7142fe81
APATARA1112 <alejandropatonaraujo2@gmail.com>
2023-10-26 17:49:45 +0200

    ej_javascript_28a terminado

---
+++ EjerciciosJavaScript/Ejercicios_21-30/Ejercicio_28/ej_javascript_28.js
@@ -0,0 +1,17 @@
+const fechaVacaciones = new Date("2023-12-19");
+
+function actualizarContador() {
+    const fecha = new Date();
+    const restaFechas = Math.floor((fechaVacaciones - fecha)/(1000*60*60*24));
+    console.log(restaFechas);
+
+    if(restaFechas>0){
+        var decenas = Math.floor(restaFechas/10);
+        var unidades = restaFechas%10;
+
+        document.getElementById("decenas").src = "imagenes/num"+decenas+".png";
+        document.getElementById("unidades").src = "imagenes/num"+unidades+".png";
+    }
+}
+
+setInterval(actualizarContador, 1000);
