620b79b27bea1cc5f76fa2121e8cee56db77c944
APATARA1112 <alejandropatonaraujo2@gmail.com>
2023-11-05 14:26:20 +0100

    ej_javascript_32

--- EjerciciosJavaScript/Ejercicios_31-42/Ejercicio_32/ej_javascript_32.js
+++ EjerciciosJavaScript/Ejercicios_31-42/Ejercicio_32/ej_javascript_32.js
@@ -55,20 +55,47 @@ function main() {
   const selectProvincias = document.getElementById("provincias");
   let cadenaProvincias = generarSelect(provinciasEspana);
   selectProvincias.innerHTML = cadenaProvincias;
+
+  document.getElementById("name").addEventListener("blur", function(){
+    comprobarNombre(this.value)
+  });
+
+  document.getElementById("direction").addEventListener("blur", function(){
+    comprobarDireccion(this.value)
+  });
+
+  document.getElementById("tel").addEventListener("blur", function(){
+    comprobarTelefono(this.value)
+  });
+
+  document.getElementById("dni").addEventListener("blur", function(){
+    comprobarDNI(this.value)
+  });
+
+  document.getElementById("email").addEventListener("blur", function(){
+    comprobarEmail(this.value)
+  });
+  
+  document.getElementById("localidad").addEventListener("blur", function(){
+    comprobarLocalidad(this.value)
+  });
 }
 
 function comprobarNombre(nombre){
     const expresionRegular = /^[A-Za-zÁáÉéÍíÓóÚúÜüñÑ\s\'-]+$/;
+    console.log(expresionRegular.test(nombre));
     return expresionRegular.test(nombre);
 }
 
 function comprobarDireccion(direccion){
     const expresionRegular = /^[A-Za-z0-9\s,.-]+$/;
+    console.log(expresionRegular.test(direccion));
     return expresionRegular.test(direccion);
 }
 
 function comprobarTelefono(telefono){
     const expresionRegular = /^[6-9]\d{8}$/;
+    console.log(expresionRegular.test(telefono));
     return expresionRegular.test(telefono);
 }
 
@@ -82,16 +109,19 @@ function comprobarDNI(dni) {
     const numeroDNI = dni.substring(0, 8); //Cogemos el numero del DNI
     const indice = numeroDNI % 23;
     const letraCalculada = letras[indice];
+    console.log(letraDNI === letraCalculada);
     return letraDNI === letraCalculada;//Devuelve true si la letra pasada coincide con la generada
 }
 
 function comprobarEmail(email){
     const expresionRegular = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-z]{2,})$/;
+    console.log(expresionRegular.test(email));
     return expresionRegular.test(email);
 }
 
 function comprobarLocalidad (localidad){
     const expresionRegular = /^[A-Za-zÁáÉéÍíÓóÚúÜüñÑ\s.'-]+$/;
+    console.log(expresionRegular.test(localidad));
     return expresionRegular.test(localidad);
 }
 
