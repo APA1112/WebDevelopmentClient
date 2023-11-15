021a7e47005d3310fccf275e7f85f6293b2a2317
Alejandro <apatara1112@g.educaand.es>
2023-11-15 10:20:10 +0100

    Ejercicio_35

--- EjerciciosJavaScript/Ejercicios_31-42/Ejercicio_35/Ejercicio_35.js
+++ EjerciciosJavaScript/Ejercicios_31-42/Ejercicio_35/Ejercicio_35.js
@@ -14,12 +14,16 @@ function main() {
   let arrayAños = generarAños(1934);
   document.getElementById("fechNacimiento").innerHTML = generarSelect(arrayAños);
   document.getElementById("aficiones").innerHTML = generarCheck(aficiones);
-  let cmpNombre = document.getElementById("name");
-  cmpNombre.addEventListener('change', comprobarCampo(cmpNombre.textContent, expresionNombre));
+  let cmpNombre = document.getElementById('name');
+  cmpNombre.addEventListener('change', function(){
+    if (comprobarCampo(cmpNombre, expresionNombre)){
+      cmpNombre.classList.remove('error');
+    }
+  });
 }
 
 function generarSelect(array) {
-  let cadenaelementos = '<option value="">-Elija una opción-</option>';
+  let cadenaelementos = '<option value="0">-Elija una opción-</option>';
   if (!Array.isArray(array)) return "Debe de ser un array";
   for (const elemento of array) {
     cadenaelementos += `<option value="${elemento}">${elemento}</option>`;
@@ -41,12 +45,16 @@ function generarAños(añoInicio, añoFin = new Date().getFullYear()) {
   return resultado;
 }
 
-function comprobarCampo(cadena, expresion) {
+function comprobarCampo(campo, expresion) {
+  let cadena = campo.value;
   if (cadena.length === 0 ){
+    campo.classList.add('error');
     alert ("No puede haber campos vacios");
     return false;
   }
   return expresion.test(cadena);
 }
-
+//Función para comprobrar todo el formulario campo a campo, si devuelve true
+//activa el botón enviar, si no lo deja inhabilitado
+function comprobarFormulario(){}
 window.addEventListener("DOMContentLoaded", main);
