43c3b4ab50ca8a42b986d492217bb15d2c53efc7
APATARA1112 <alejandropatonaraujo2@gmail.com>
2023-12-08 14:23:55 +0100

    quiniela

---
+++ Quiniela/quiniela.js
@@ -0,0 +1,34 @@
+let cadenaDatos = "1,2,3,4,5,6,7,8,9";
+function main(){
+    let tabla = crearTabla(7,7);
+    let datos = prepararDatos(cadenaDatos, ',');
+    rellenarTabla(tabla, datos);
+}
+//Le pasamos a la funcion el numero de filas y columnas que queremos que tenga la tabla y nos la crea
+//usando los metodos del createElement
+function crearTabla(filas, columnas) {
+  var tabla = document.createElement("table");
+  for (var i = 0; i < filas; i++) {
+    var fila = tabla.insertRow(i);
+    for (var j = 0; j < columnas; j++) {
+      fila.insertCell(j);
+    }
+  }
+  document.body.appendChild(tabla);
+  return tabla;
+}
+function prepararDatos(cadenaDatos, separador){
+    return cadenaDatos.split(separador);
+}
+
+function rellenarTabla(tabla, datos){
+    var filas = tabla.rows.length;
+    var columnas = datos.length / filas;
+    for (var i = 0; i < filas; i++) {
+        for (var j = 0; j < columnas; j++) {
+          tabla.rows[i].cells[j].textContent = datos[i * columnas + j];
+        }
+      }
+}
+
+window.addEventListener("DOMContentLoaded", main);
