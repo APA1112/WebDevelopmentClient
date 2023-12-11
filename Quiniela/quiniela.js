9082d9f910df16404f9b1cb3836c640a294675f2
Alejandro <apatara1112@g.educaand.es>
2023-12-11 13:28:28 +0100

    quiniela

--- Quiniela/quiniela.js
+++ Quiniela/quiniela.js
@@ -1,8 +1,11 @@
-let cadenaDatos = "1,2,3,4,5,6,7,8,9";
-function main(){
-    let tabla = crearTabla(7,7);
-    let datos = prepararDatos(cadenaDatos, ',');
-    rellenarTabla(tabla, datos);
+let cadenaDatos =
+  "1,2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 1,2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 1,2,3,4,5,6,7,8, 47, 48, 49";
+function main() {
+  x = document.getElementsByTagName("td");
+  console.log(x);
+  let tabla = crearTabla(7, 7);
+  let datos = prepararDatos(cadenaDatos, ",");
+  rellenarTabla(tabla, datos);
 }
 //Le pasamos a la funcion el numero de filas y columnas que queremos que tenga la tabla y nos la crea
 //usando los metodos del createElement
@@ -17,18 +20,18 @@ function crearTabla(filas, columnas) {
   document.body.appendChild(tabla);
   return tabla;
 }
-function prepararDatos(cadenaDatos, separador){
-    return cadenaDatos.split(separador);
+function prepararDatos(cadenaDatos, separador) {
+  return cadenaDatos.split(separador);
 }
 
-function rellenarTabla(tabla, datos){
-    var filas = tabla.rows.length;
-    var columnas = datos.length / filas;
-    for (var i = 0; i < filas; i++) {
-        for (var j = 0; j < columnas; j++) {
-          tabla.rows[i].cells[j].textContent = datos[i * columnas + j];
-        }
-      }
+function rellenarTabla(tabla, datos) {
+  var filas = tabla.rows.length;
+  var columnas = datos.length / filas;
+  for (var i = 0; i < filas; i++) {
+    for (var j = 0; j < columnas; j++) {
+      tabla.rows[i].cells[j].textContent = datos[i * columnas + j];
+    }
+  }
 }
 
 window.addEventListener("DOMContentLoaded", main);
