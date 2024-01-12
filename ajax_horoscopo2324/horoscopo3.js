f91cfc30f10c178fb18b23668b10ff985faed5f9
Alejandro <apatara1112@g.educaand.es>
2024-01-12 09:24:31 +0100

    Selecting button

--- ajax_horoscopo2324/horoscopo3.js
+++ ajax_horoscopo2324/horoscopo3.js
@@ -1,8 +1,25 @@
-function reqListener(){
-    console.log(this.responseText);
+function reqListener() {
+  console.log(this.responseText);
 }
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://localhost/cliente/bd_ajax.php?numero=1");
+oReq.open("GET", "http://localhost/cliente/bd_ajax.php?");
 oReq.send();
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  const nodosBotones = document.querySelectorAll("asside button");
+
+  nodosBotones.forEach((ele, ind)=>{
+    console.log(ele, ind);
+    ele.addEventListener("click", function(){
+        buscarInfo(ele, ind);
+    });
+  });
+}
+
+function buscarInfo(e, i) {
+  console.log(e, i);
+}
