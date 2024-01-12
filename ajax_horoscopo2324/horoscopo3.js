244c33f8f0df46647f78668aaeaa0a20abe94889
Alejandro <apatara1112@g.educaand.es>
2024-01-12 09:50:35 +0100

    horoscopo3.js

--- ajax_horoscopo2324/horoscopo3.js
+++ ajax_horoscopo2324/horoscopo3.js
@@ -1,25 +1,31 @@
-function reqListener() {
-  console.log(this.responseText);
-}
-
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://localhost/cliente/bd_ajax.php?");
-oReq.send();
-
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
   const nodosBotones = document.querySelectorAll("asside button");
 
-  nodosBotones.forEach((ele, ind)=>{
+  nodosBotones.forEach((ele, ind) => {
     console.log(ele, ind);
-    ele.addEventListener("click", function(){
-        buscarInf(ele, ind);
+    ele.addEventListener("click", function () {
+      buscarInf(ele, ind);
     });
   });
 }
 
 function buscarInf(e, i) {
-  console.log(e, i);
+  console.log(e.target.textContent, i);
+
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", `http://localhost/cliente/db_ajax.php?numero=${i}`);
+  oReq.send();
+}
+
+function reqListener() {
+  let datos = JSON.parse(this.responseText);
+  console.log(datos);
+  nodoInsertar = document.getElementById("idtexto");
+  nodoInsertar.innerHTML="";
+  for (const property in datos){
+    nodoInsertar.innerHTML+=(`${property}: ${datos[property]}`);
+  }
 }
