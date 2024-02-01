2062fd84ed7976be2c5523fe393efe60afefea3c
APATARA1112 <alejandropatonaraujo2@gmail.com>
2024-02-01 18:33:08 +0100

    Ejercicio react

--- comentarios/src/Comentario.jsx
+++ comentarios/src/Comentario.jsx
@@ -1,11 +1,13 @@
+import styles from "./Comentario.module.css";
+
 export function Comentario(props) {
   return (
-    <div class="comentario">
-      <img src="./src/img/foto1.png" alt="foto" />
-      <div className="comentario_contenedor">
-        <p class="comentario_parrafo">{props.nombre}</p>
-        <p class="comentario_parrafo">{props.trabajo}</p>
-        <p class="comentario_texto">{props.comentarios}</p>
+    <div className={styles.comentario}>
+      <img className={styles.imagen} src="./src/img/foto1.png" alt="foto" />
+      <div className={styles.comentario_contenedor}>
+        <p className={styles.comentario_contenedor}>{props.nombre}</p>
+        <p className={styles.comentario_contenedor}>{props.trabajo}</p>
+        <p className={styles.comentario_contenedor}>{props.comentarios}</p>
       </div>
     </div>
   )
