875656c4e65730caba00fab996ffadc63defadbf
Alejandro <apatara1112@g.educaand.es>
2024-02-02 08:44:01 +0100

    Modulo css e imagen con import

--- comentarios/src/App.jsx
+++ comentarios/src/App.jsx
@@ -8,16 +8,19 @@ function App() {
         <h2>Here is what our alumni say about freeCodeCamp</h2>
       </header>
       <Comentario
+        foto="foto1.png"
         nombre="botijo1"
         trabajo="En mi casa los domingos por la mañana"
         comentario="Lorem ipsum"
       ></Comentario>
       <Comentario
+        foto="foto2.png"
         nombre="botijo2"
         trabajo="En mi casa los domingos por la mañana"
         comentario="Lorem ipsum"
       ></Comentario>
       <Comentario
+        foto="foto3.png"
         nombre="botijo3"
         trabajo="En mi casa los domingos por la mañana"
         comentario="Lorem ipsum"
--- comentarios/src/Comentario.jsx
+++ comentarios/src/Comentario.jsx
@@ -1,9 +1,10 @@
 import styles from "./Comentario.module.css";
+import imagen1 from "./img/foto1.png";
 
 export function Comentario(props) {
   return (
     <div className={styles.comentario}>
-      <img className={styles.imagen} src="./src/img/foto1.png" alt="foto" />
+      <img className={styles.imagen} src={imagen1} alt="foto" />
       <div className={styles.comentario_contenedor}>
         <p className={styles.comentario_contenedor}>{props.nombre}</p>
         <p className={styles.comentario_contenedor}>{props.trabajo}</p>
